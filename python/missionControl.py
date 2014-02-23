221fe33874f9158b4f1eefc39a0c603dd8bab1ca
Greg Berezowsky <greg.berezowsky@gmail.com>
2014-02-23 14:57:47 +0000

    Move the baud rate to a variable

--- python/missionControl.py
+++ python/missionControl.py
@@ -3,8 +3,9 @@ import serial
 import threading
 
 serialDevice = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_741333534373512161B1-if00"
+baudRate = 115200
 
-arduinoSerial = serial.Serial(serialDevice, 115200)
+arduinoSerial = serial.Serial(serialDevice, baudRate)
 arduinoSerial.flush()
 
 def mainLoop():
