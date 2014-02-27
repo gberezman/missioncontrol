66c19770f8f8c67cd43b7a6d60d5b68bd13136c2
Greg Berezowsky <greg.berezowsky@gmail.com>
2014-02-27 16:26:27 +0000

    Add basic 7 segment led test

--- arduino/SimpleTests.ino
+++ arduino/SimpleTests.ino
@@ -1,13 +1,25 @@
 const int LED_EXTERNAL = 22;
 const int SWITCH = 52;
 
-boolean wasSwitchOpen;
-
 int switchState;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_EXTERNAL, OUTPUT);
+  
+  // Enable segment LED pins and digit pins for output
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(A6, OUTPUT);
+  pinMode(A7, OUTPUT);
+  pinMode(A8, OUTPUT);
+  pinMode(A9, OUTPUT);
+  pinMode(A10, OUTPUT);
+  pinMode(A11, OUTPUT);
+  pinMode(A12, OUTPUT);
   
   switchState = digitalRead(SWITCH);
   
@@ -17,6 +29,8 @@ void setup() {
 void loop() {
   processSerial();
   processSwitch();
+  processLed();
+  delay(20);
 }
 
 void processSerial(void) {
@@ -51,3 +65,16 @@ void processSwitch(void) {
       Serial.write('x');
   }
 }
+
+void processLed(void) {
+  digitalWrite(A10, HIGH);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, LOW);
+  digitalWrite(A6, LOW);
+  digitalWrite(A7, LOW);
+  digitalWrite(A8, LOW);
+}
+
