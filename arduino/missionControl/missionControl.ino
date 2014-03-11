9f0998dd005bf6cd71e9f7ba70d396d63ffffb7a
greg.berezowsky <greg.berezowsky@gmail.com>
2014-03-11 12:20:56 +0000

    Some refactoring of arduino mission control code to handle multiple I/O expanders

--- arduino/missionControl/missionControl.ino
+++ arduino/missionControl/missionControl.ino
@@ -1,47 +1,70 @@
 #include <Wire.h>
-#include "Adafruit_MCP23017.h"
+#include "./Adafruit_MCP23017.h"
 
 Adafruit_MCP23017 mcp0;
+Adafruit_MCP23017 mcp1;
+Adafruit_MCP23017 mcp2;
+Adafruit_MCP23017 mcp3;
+Adafruit_MCP23017 mcps[] = { mcp0 };
+// Adafruit_MCP23017 mcps[] = { mcp0, mcp1, mcp2, mcp3 };
 
-uint8_t state[64];
+#define NUM_EXPANDER_PINS 16
+#define NUM_EXPANDERS sizeof(mcps)/sizeof(Adafruit_MCP23017)
+#define NUM_SWITCHES ( NUM_EXPANDER_PINS ) * ( NUM_EXPANDERS )
+
+uint8_t switchStates[NUM_SWITCHES];
   
-void setup() {  
+void setup() {
   Serial.begin(115200);
- 
-  for( int i = 0; i < 64; i++ ) {
-    state[i] = 0;
-  }
-
-  mcp0.begin(0);
-
-  for( int pin = 0; pin < 16; pin++ ) { 
-    mcp0.pinMode(pin, INPUT);
-    mcp0.pullUp(pin, HIGH);  // 100K pullup 
-  }
+  initializeBuffers();
+  for( int mcp = 0; mcp < NUM_EXPANDERS; mcp++ )
+    initializeExpander(mcps[mcp], mcp);
 }
 
 void loop() {
   scanSwitches();
 }
 
-void scanSwitches() {
-  byte current[16];
-  uint16_t mcp0States = mcp0.readGPIOAB();
-  for( int i = 0; i < 16; i++ ) {
-    if( mcp0States & (1 << i) ) {
-      current[i] = 1;
-    } else {
-      current[i] = 0;
-    }
-  }
+void initializeBuffers() {
+  for( int i = 0; i < NUM_SWITCHES; i++ ) 
+    switchStates[i] = 0;
+}
 
-  for( int i = 0; i < 16; i++ ) {
-    if( ( current[i] == 0 ) && ( state[i] == 1 ) ) {
+void initializeExpander(Adafruit_MCP23017 mcp, int address) {
+  mcp.begin(address);
+
+  for( int pin = 0; pin < NUM_EXPANDER_PINS; pin++ ) { 
+    mcp.pinMode(pin, INPUT);
+    mcp.pullUp(pin, HIGH);  // 100K pullup 
+  }
+}
+
+void scanSwitches() {
+  uint8_t current[NUM_SWITCHES];
+  
+  for( int mcp = 0; mcp < NUM_EXPANDERS; mcp++ )
+    scanMCPSwitches(mcps[mcp], current, 16 * mcp);
+  
+  sendAndUpdateSwitchStateChanges(current);
+}
+
+void scanMCPSwitches(Adafruit_MCP23017 mcp, uint8_t states[], int offset) {
+  uint16_t gpioState = mcp.readGPIOAB();
+  for( int i = 0; i < NUM_EXPANDER_PINS; i++ )
+    states[i + offset] = ( gpioState & (1 << i) ) >> i;
+}
+
+void sendAndUpdateSwitchStateChanges(uint8_t current[]) {
+  for( int i = 0; i < NUM_SWITCHES; i++ ) {
+    boolean onToOff = ( switchStates[i] == 1 ) && ( current[i] == 0 );
+    boolean offToOn = ( switchStates[i] == 0 ) && ( current[i] == 1 );
+    if( onToOff ) {
         Serial.write(i + 128);
-        state[i] = current[i];
-    } else if ( ( current[i] == 1 ) && ( state[i] == 0 ) ) {
+        switchStates[i] = current[i];
+    } else if ( offToOn ) {
         Serial.write(i);
-        state[i] = current[i];
+        switchStates[i] = current[i];
     }
   }
 }
+
