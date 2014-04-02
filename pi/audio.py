7ee90cc350b9ba89809f93598420841b68ecdca2
greg.berezowsky <greg.berezowsky@gmail.com>
2014-04-02 02:34:57 +0000

    Reserve two channels for the long running event sequence and caution audio.

--- pi/audio.py
+++ pi/audio.py
@@ -7,6 +7,10 @@ class Audio:
         pygame.mixer.quit()
         pygame.mixer.init( frequency = 48000, buffer = 1024 )
 
+        pygame.mixer.set_reserved( 2 )
+        self.cautionChannel = pygame.mixer.Channel(0)
+        self.ESChannel      = pygame.mixer.Channel(1)
+
         self.quindarin   = pygame.mixer.Sound( 'audio/quindar-in.wav' )
         self.quindarout  = pygame.mixer.Sound( 'audio/quindar-out.wav' )
         self.spsThruster = pygame.mixer.Sound( 'audio/rocket.wav' )
@@ -14,3 +18,5 @@ class Audio:
         self.o2fan       = pygame.mixer.Sound( 'audio/o2fan.wav' )
         self.h2fan       = pygame.mixer.Sound( 'audio/h2fan.wav' )
         self.csmDeploy   = pygame.mixer.Sound( 'audio/csmDeploy.wav' )
+        self.ES1         = pygame.mixer.Sound( 'audio/ES1.wav' )
+        self.ES2         = pygame.mixer.Sound( 'audio/ES2.wav' )
--- pi/rules.py
+++ pi/rules.py
@@ -9,8 +9,15 @@ class Rules:
     def sendMeterSetting(self, port, meterId, meterValue):
         print "sending meter {} = {}".format( meterId, meterValue )
         port.write( "Meter " + meterID + " " + str(meterValue) + "\n" )
-        return True
 
+    def playES(self, sound):
+        self.audio.esChannel.stop() 
+        self.audio.esChannel.play( sound )
+
+    def playCaution(self, sound):
+        self.audio.cautionChannel.stop() 
+        self.audio.cautionChannel.play( sound )
+        
     def __init__(self):
         self.audio = Audio()
 
@@ -23,7 +30,7 @@ class Rules:
             'AbortMode'  : lambda port, potValue: noAction,
 
             # EECOM
-            'Voltage'    : lambda port, potValue: self.sendMeterSetting(port, "Voltage", 12 - potValue)
+            'Voltage'    : lambda port, potValue: self.sendMeterSetting(port, "Voltage", 12 - potValue),
             'Current'    : lambda port, potValue: noAction,
             'Resistance' : lambda port, potValue: noAction,
             'O2Flow'     : lambda port, potValue: self.sendMeterSetting(port, "O2Flow", 12 - potValue),
@@ -38,125 +45,125 @@ class Rules:
         self.onRule = {
             # CONTROL
             'DockingProbe'    : lambda : self.audio.spsThruster.play(loops = -1),
-            'GlycolPump'      : lambda: noAction,
-            'SCEPower'        : lambda: noAction,
-            'WasteDump'       : lambda: noAction,
+            'GlycolPump'      : lambda : noAction,
+            'SCEPower'        : lambda : noAction,
+            'WasteDump'       : lambda : noAction,
             'CabinFan'        : lambda : self.audio.fan.play(loops = -1),
-            'H2OFlow'         : lambda: noAction,
-            'IntLights'       : lambda: noAction,
-            'SuitComp'        : lambda: noAction,
+            'H2OFlow'         : lambda : noAction,
+            'IntLights'       : lambda : noAction,
+            'SuitComp'        : lambda : noAction,
 
             # ABORT
-            'ArmAbort'        : lambda: noAction,
-            'Abort'           : lambda: noAction,
+            'ArmAbort'        : lambda : noAction,
+            'Abort'           : lambda : noAction,
 
             # BOOSTER
             'SPS'             : lambda : self.audio.spsThruster.play(loops = -1),
-            'TEI'             : lambda: noAction,
-            'TLI'             : lambda: noAction,
-            'S-IC'            : lambda: noAction,
-            'S-II'            : lambda: noAction,
-            'S-iVB'           : lambda: noAction,
-            'M-I'             : lambda: noAction,
-            'M-II'            : lambda: noAction,
-            'M-III'           : lambda: noAction,
+            'TEI'             : lambda : noAction,
+            'TLI'             : lambda : noAction,
+            'S-IC'            : lambda : noAction,
+            'S-II'            : lambda : noAction,
+            'S-iVB'           : lambda : noAction,
+            'M-I'             : lambda : noAction,
+            'M-II'            : lambda : noAction,
+            'M-III'           : lambda : noAction,
 
             # C&WS
-            'Power'           : lambda: noAction,
-            'Mode'            : lambda: noAction,
-            'Lamp'            : lambda: noAction,
-            'Ack'             : lambda: noAction,
+            'Power'           : lambda : noAction,
+            'Mode'            : lambda : noAction,
+            'Lamp'            : lambda : noAction,
+            'Ack'             : lambda : noAction,
 
             # CAPCOM
             'PTT'             : lambda : self.audio.quindarin.play(),
 
             # EVENT SEQUENCE
-            'ES1'             : lambda: noAction,
-            'ES2'             : lambda: noAction,
-            'ES3'             : lambda: noAction,
-            'ES4'             : lambda: noAction,
-            'ES5'             : lambda: noAction,
-            'ES6'             : lambda: noAction,
-            'ES7'             : lambda: noAction,
-            'ES8'             : lambda: noAction,
-            'ES9'             : lambda: noAction,
-            'ES10'            : lambda: noAction,
+            'ES1'             : lambda : playES( self.audio.ES1 ),
+            'ES2'             : lambda : playES( self.audio.ES2 ),
+            'ES3'             : lambda : noAction,
+            'ES4'             : lambda : noAction,
+            'ES5'             : lambda : noAction,
+            'ES6'             : lambda : noAction,
+            'ES7'             : lambda : noAction,
+            'ES8'             : lambda : noAction,
+            'ES9'             : lambda : noAction,
+            'ES10'            : lambda : noAction,
 
             # CRYOGENICS
             'O2Fan'           : lambda : self.audio.o2fan.play(loops = -1),
             'H2Fan'           : lambda : self.audio.h2fan.play(loops = -1),
-            'Pumps'           : lambda: noAction,
-            'Heat'            : lambda: noAction,
+            'Pumps'           : lambda : noAction,
+            'Heat'            : lambda : noAction,
 
             # PYROTECHNICS
-            'MainDeploy'      : lambda: noAction,
+            'MainDeploy'      : lambda : noAction,
             'CSM/LVDeploy'    : lambda : self.audio.csmDeploy.play(),
-            'SM/CMDeploy'     : lambda: noAction,
-            'DrogueDeploy'    : lambda: noAction,
-            'CanardDeploy'    : lambda: noAction,
-            'ApexCoverJettsn' : lambda: noAction,
-            'LesMotorFire'    : lambda: noAction
+            'SM/CMDeploy'     : lambda : noAction,
+            'DrogueDeploy'    : lambda : noAction,
+            'CanardDeploy'    : lambda : noAction,
+            'ApexCoverJettsn' : lambda : noAction,
+            'LesMotorFire'    : lambda : noAction
         }
 
         self.offRule = {
             # CONTROL
-            'DockingProbe'    : lambda: noAction,
-            'GlycolPump'      : lambda: noAction,
-            'SCEPower'        : lambda: noAction,
-            'WasteDump'       : lambda: noAction,
+            'DockingProbe'    : lambda : noAction,
+            'GlycolPump'      : lambda : noAction,
+            'SCEPower'        : lambda : noAction,
+            'WasteDump'       : lambda : noAction,
             'CabinFan'        : lambda : self.audio.fan.stop(),
-            'H2OFlow'         : lambda: noAction,
-            'IntLights'       : lambda: noAction,
-            'SuitComp'        : lambda: noAction,
+            'H2OFlow'         : lambda : noAction,
+            'IntLights'       : lambda : noAction,
+            'SuitComp'        : lambda : noAction,
 
             # ABORT
-            'ArmAbort'        : lambda: noAction,
-            'Abort'           : lambda: noAction,
+            'ArmAbort'        : lambda : noAction,
+            'Abort'           : lambda : noAction,
 
             # BOOSTER
             'SPS'               : lambda : self.audio.spsThruster.stop(),
-            'TEI'             : lambda: noAction,
-            'TLI'             : lambda: noAction,
-            'S-IC'            : lambda: noAction,
-            'S-II'            : lambda: noAction,
-            'S-iVB'           : lambda: noAction,
-            'M-I'             : lambda: noAction,
-            'M-II'            : lambda: noAction,
-            'M-III'           : lambda: noAction,
+            'TEI'             : lambda : noAction,
+            'TLI'             : lambda : noAction,
+            'S-IC'            : lambda : noAction,
+            'S-II'            : lambda : noAction,
+            'S-iVB'           : lambda : noAction,
+            'M-I'             : lambda : noAction,
+            'M-II'            : lambda : noAction,
+            'M-III'           : lambda : noAction,
 
             # C&WS
-            'Power'           : lambda: noAction,
-            'Mode'            : lambda: noAction,
-            'Lamp'            : lambda: noAction,
-            'Ack'             : lambda: noAction,
+            'Power'           : lambda : noAction,
+            'Mode'            : lambda : noAction,
+            'Lamp'            : lambda : noAction,
+            'Ack'             : lambda : noAction,
 
             # CAPCOM
             'PTT'             : lambda : self.audio.quindarout.play(),
 
             # EVENT SEQUENCE
-            'ES1'             : lambda: noAction,
-            'ES2'             : lambda: noAction,
-            'ES3'             : lambda: noAction,
-            'ES4'             : lambda: noAction,
-            'ES5'             : lambda: noAction,
-            'ES6'             : lambda: noAction,
-            'ES7'             : lambda: noAction,
-            'ES8'             : lambda: noAction,
-            'ES9'             : lambda: noAction,
-            'ES10'            : lambda: noAction,
+            'ES1'             : lambda : noAction,
+            'ES2'             : lambda : noAction,
+            'ES3'             : lambda : noAction,
+            'ES4'             : lambda : noAction,
+            'ES5'             : lambda : noAction,
+            'ES6'             : lambda : noAction,
+            'ES7'             : lambda : noAction,
+            'ES8'             : lambda : noAction,
+            'ES9'             : lambda : noAction,
+            'ES10'            : lambda : noAction,
 
             # CRYOGENICS
             'O2Fan'           : lambda : self.audio.o2fan.stop(),
             'H2Fan'           : lambda : self.audio.h2fan.stop()
-            'Pumps'           : lambda: noAction,
-            'Heat'            : lambda: noAction,
+            'Pumps'           : lambda : noAction,
+            'Heat'            : lambda : noAction,
 
             # PYROTECHNICS
-            'MainDeploy'      : lambda: noAction,
-            'CSM/LVDeploy'    : lambda: noAction,
-            'SM/CMDeploy'     : lambda: noAction,
-            'DrogueDeploy'    : lambda: noAction,
-            'CanardDeploy'    : lambda: noAction,
-            'ApexCoverJettsn' : lambda: noAction,
-            'LesMotorFire'    : lambda: noAction
+            'MainDeploy'      : lambda : noAction,
+            'CSM/LVDeploy'    : lambda : noAction,
+            'SM/CMDeploy'     : lambda : noAction,
+            'DrogueDeploy'    : lambda : noAction,
+            'CanardDeploy'    : lambda : noAction,
+            'ApexCoverJettsn' : lambda : noAction,
+            'LesMotorFire'    : lambda : noAction
         }
