3535ccccdb50beb9f70c0244f90c74e419c6ef7f
greg.berezowsky <greg.berezowsky@gmail.com>
2014-04-15 14:09:04 +0000

    Add empty test structure for rules

---
+++ pi/missionControl/test/test_rules.py
@@ -0,0 +1,16 @@
+from missionControl.rules import CautionWarning, Abort, EventRecord, Rules
+
+class TestCautionWarning:
+    pass
+
+class TestAbort:
+    pass
+
+class TestEventRecord:
+    pass
+
+class TestLatchedLED:
+    pass
+
+class TestRules:
+    pass
