96c97aa66f036047021a22780a4c94c140ea3b31
greg.berezowsky <greg.berezowsky@gmail.com>
2014-04-17 15:20:02 +0000

    Update latched LED tests to use fixtures

--- pi/missionControl/tests/test_latchedLED_rules.py
+++ pi/missionControl/tests/test_latchedLED_rules.py
@@ -4,49 +4,44 @@ from missionControl.arduino import StubbedArduinoSerial, ArduinoMatrixDriver
 
 class TestLatchedLED:
 
-    def test_on_incrementsButtonCount(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
+    @pytest.fixture
+    def serial(self):
+        return StubbedArduinoSerial()
 
+    @pytest.fixture
+    def latch(self, serial):
+        return LatchedLED( ArduinoMatrixDriver( serial ), 'Button' )
+
+    def test_on_incrementsButtonCount(self, latch):
         latch.on()
 
         assert latch.buttonCount == 1
 
-    def test_on_decrementsButtonCount_ifNotOn(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
+    def test_on_decrementsButtonCount_ifNotOn(self, latch):
         latch.on()
+
         latch.on( False )
 
         assert latch.buttonCount == 0
 
-    def test_off_decrementsButtonCount(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
+    def test_off_decrementsButtonCount(self, latch):
         latch.on()
         latch.off()
 
         assert latch.buttonCount == 0
 
-    def test_off_doesNotdecrementBelowZero(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
+    def test_off_doesNotdecrementBelowZero(self, latch):
         latch.off()
         latch.off()
 
         assert latch.buttonCount == 0
 
-    def test_on_enablesLed(self):
-        serial = StubbedArduinoSerial()
-        latch = LatchedLED( ArduinoMatrixDriver( serial ), 'Button' )
-
+    def test_on_enablesLed(self, serial, latch):
         latch.on()
 
         serial.getLastWrite() == 'LED Button on\n'
 
-    def test_on_disablesLed(self):
-        serial = StubbedArduinoSerial()
-        latch = LatchedLED( ArduinoMatrixDriver( serial ), 'Button' )
-
+    def test_on_disablesLed(self, serial, latch):
         latch.on()
         latch.off()
 
