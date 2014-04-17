bc0107a9a605059b29e5df715ccd7942f76148d7
greg.berezowsky <greg.berezowsky@gmail.com>
2014-04-17 15:15:13 +0000

    Move latched LED tests to their own file

--- pi/missionControl/tests/test_rules.py
+++ pi/missionControl/tests/test_rules.py
@@ -1,58 +1,8 @@
 from time import sleep
 import pytest
-from missionControl.rules import Rules, LatchedLED
-from missionControl.audio import Audio, StubbedAudio
+from missionControl.rules import Rules
+from missionControl.audio import StubbedAudio
 from missionControl.arduino import StubbedArduinoSerial, ArduinoMatrixDriver
-
-class TestLatchedLED:
-
-    def test_on_incrementsButtonCount(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
-        latch.on()
-
-        assert latch.buttonCount == 1
-
-    def test_on_decrementsButtonCount_ifNotOn(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
-        latch.on()
-        latch.on( False )
-
-        assert latch.buttonCount == 0
-
-    def test_off_decrementsButtonCount(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
-        latch.on()
-        latch.off()
-
-        assert latch.buttonCount == 0
-
-    def test_off_doesNotdecrementBelowZero(self):
-        latch = LatchedLED( ArduinoMatrixDriver( StubbedArduinoSerial() ), 'Button' )
-
-        latch.off()
-        latch.off()
-
-        assert latch.buttonCount == 0
-
-    def test_on_enablesLed(self):
-        serial = StubbedArduinoSerial()
-        latch = LatchedLED( ArduinoMatrixDriver( serial ), 'Button' )
-
-        latch.on()
-
-        serial.getLastWrite() == 'LED Button on\n'
-
-    def test_on_disablesLed(self):
-        serial = StubbedArduinoSerial()
-        latch = LatchedLED( ArduinoMatrixDriver( serial ), 'Button' )
-
-        latch.on()
-        latch.off()
-
-        serial.getLastWrite() == 'LED Button off\n'
 
 class TestRules:
 
