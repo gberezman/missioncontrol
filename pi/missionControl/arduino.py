e4625d31d09271c752a240fa02470de0d9e9f8f4
Greg Berezowsky <greg.berezowsky@gmail.com>
2014-05-16 19:17:55 +0000

    Add arduino driver commands for controlling inco meter

--- pi/missionControl/arduino.py
+++ pi/missionControl/arduino.py
@@ -55,8 +55,11 @@ class ArduinoMatrixDriver:
     def setNumber(self, numberId, value):
         self.serial.write( "N {} {}\n".format( numberId, value ) )
 
-    def setInco(self, value):
-        self.serial.write( "I {}\n".format( value ) )
+    def setInco(self, lo, hi):
+        self.serial.write( "I {} {}\n".format( lo, hi ) )
+
+    def setIncoColor(self, values):
+        self.serial.write( "C {}\n".format( values ) )
 
     def ledOn(self, ledId):
         self.serial.write( "L {} 1\n".format( ledId ) )
